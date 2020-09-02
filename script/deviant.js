009732d9039781f1f28c923a30b144674bf04539
Liz Kane <lizkane@Lizs-MBP.attlocal.net>
2020-09-02 00:53:18 -0700

    created user profile area, flex-boxed each section

--- script/deviant.js
+++ script/deviant.js
@@ -1,12 +1,19 @@
 // Get the modal
 
-let $modal = 
 
 
 // let $encryption = $('#encryption')
 
+$('#backstory a').hover(function(){
+    $(this).css("color", "#90e0ef");
+    $(this).css("background-color","yellow");
+    }, 
+    function(){
+    $(this).css("background-color", "#03045e");
+  });
 
-// // Get the modal
+
+// Get the modal
 // let modal = document.getElementById("myModal");
 
 // // Get the image and insert it inside the modal - use its "alt" text as a caption
@@ -20,17 +27,49 @@ let $modal =
 //     $("#caption").text = (this).alt;
 // });
 
-$('.trigger').hover(function(){
-    $(".myModal").css("display", "block");
+
+
+// class triggerModal {
+//     constructor(triggerID, imgNumID, captionID, ){
+//         this.modal = $('.modal');
+//         this.myModal = $('.myModal')
+//         this.triggerClass= $('.trigger');
+//         this.triggerID = triggerID
+//         this.imgNumID = imgNumID;
+//         this.captionID = captionID;
+//     }
+//     modalMaker() {
+//         $(this.triggerID).hover(function(){
+//             $(this.modal).css("display", "block");
+//             $(this.imgNumID).src = (this).src;
+//             $(this.captionID).text = (this).alt;
+//             $(this.triggerClass).css("color", "#90e0ef");
+//             $(this.triggerClass).css("background-color","gray");      
+//             }, 
+//             function(){
+//             $(this.triggerClass).css("background-color", "#03045e");
+//             $(this.myModal).css("display", "none");
+//     });
+//     }
+// }
+
+// const phantomWars = new triggerModal($('#phantomWars'),$('#img00'),$('#caption00'));
+
+// // phantomWars.modalMaker()
+
+
+
+$('#firstTrigger').hover(function(){
+    $(".modal").css("display", "block");
     $("img01").src = (this).src;
-    $("#caption").text = (this).alt;
+    // $("#caption01").text = (this).alt;
     $(this).css("color", "#90e0ef");
     $(this).css("background-color","gray");
     
     }, 
     function(){
     $(this).css("background-color", "#03045e");
-    $('.modal').css("display", "none");
+    $('.myModal').css("display", "none");
 });
 
 
